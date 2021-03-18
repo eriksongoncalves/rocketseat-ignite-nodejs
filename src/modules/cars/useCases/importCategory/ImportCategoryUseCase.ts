0f64dba955d0e60c909796f2581af14ea2595cec
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-03-18 17:23:29 -0300

    feat: useCase import category

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,8 @@
+class ImportCategoryUseCase {
+  execute(file: Express.Multer.File) {
+    // eslint-disable-next-line no-console
+    console.log(file);
+  }
+}
+
+export default ImportCategoryUseCase;
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 
 const categoriesRouter = Router();
+
+const upload = multer({
+  dest: './tmp'
+});
 
 categoriesRouter.post('/', (req, res) => {
   return createCategoryController.handle(req, res);
@@ -11,6 +17,10 @@ categoriesRouter.post('/', (req, res) => {
 
 categoriesRouter.get('/', (req, res) => {
   return listCategoriesController.handle(req, res);
+});
+
+categoriesRouter.post('/import', upload.single('file'), (req, res) => {
+  return importCategoryController.handle(req, res);
 });
 
 export default categoriesRouter;
