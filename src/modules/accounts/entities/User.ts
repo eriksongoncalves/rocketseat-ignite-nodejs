c002690870d4f3061057bdcd8419c4a67a293d04
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-03-23 16:36:10 -0300

    feat: entity user

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,46 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn
+} from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+@Entity('users')
+class User {
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  driver_license: string;
+
+  @Column()
+  isAdmin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default User;
